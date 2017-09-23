338e74f8c9c585d3b7ddee42297914ecd0043987
Shynggys96 <31817524+Shynggys96@users.noreply.github.com>
2017-09-23 17:52:04 +0300

    Update 0.cpp
    Сделал круче

--- 0.cpp
+++ 0.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
-#include <string>
-
-int 
-main(void)
-{
-  std::string str;
-  str = "Hello, world!";
-  std::cout << str << std::endl;
-  return (1==0);
+ 
+int main() {
+	char *hello = new char[14];
+	hello[0] = 'H';
+	hello[1] = 'e';
+	hello[2] = 'l';
+	hello[3] = 'l';
+	hello[4] = 'o';
+	hello[5] = ',';
+	hello[6] = ' ';
+	hello[7] = 'w';
+	hello[8] = 'o';
+	hello[9] = 'r';
+	hello[10] = 'l';
+	hello[11] = 'd';
+	hello[12] = '!';
+	hello[13] = '\0';
+	std::cout << hello << std::endl;
+	return (2 + 2 == 5);
 }
